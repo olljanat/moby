1fa4d43cc65bb5a38a65499f409ad272da01d0f1
Olli Janatuinen <olli.janatuinen@gmail.com>
2019-11-27 14:50:44 +0200

    DO NOT MERGE: Start of custom release process

---
+++ build.ps1
@@ -0,0 +1,9 @@
+$VERSION = "v19.03.13-olljanat1"
+
+docker build -t nativebuildimage -f Dockerfile.windows .
+$DOCKER_GITCOMMIT=(git rev-parse --short HEAD)
+docker run --name binaries -e DOCKER_GITCOMMIT=$DOCKER_GITCOMMIT -e VERSION=$VERSION nativebuildimage hack\make.ps1 -Binary
+docker cp binaries:C:\go\src\github.com\docker\docker\bundles\dockerd.exe C:\HostPath\dockerd.exe
+docker rm binaries
+
+Write-Host "Build time $(Get-Date -Format u)"
