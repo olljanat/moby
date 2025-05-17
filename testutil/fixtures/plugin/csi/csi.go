1b0a763ba3b51006ab6a3e756e06cf2fa9af0dbf
Olli Janatuinen <olli.janatuinen@gmail.com>
2025-05-17 12:29:33 +0000

    Store draft which is NOT compatible with shim

--- testutil/fixtures/plugin/csi/csi.go
+++ testutil/fixtures/plugin/csi/csi.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"context"
-	"net"
-	"os"
-	"path/filepath"
 
+	"github.com/cloudflare/cfssl/log"
 	"github.com/docker/go-plugins-helpers/volume"
 	volumeplugin "github.com/docker/go-plugins-helpers/volume"
+	"github.com/docker/go-plugins-helpers/volume/shim"
 
 	csi "github.com/container-storage-interface/spec/lib/go/csi"
-	"google.golang.org/grpc"
 )
 
 type driverServer struct {
+	d volume.Driver
 	csi.UnimplementedIdentityServer
 	csi.UnimplementedControllerServer
 	csi.UnimplementedNodeServer
@@ -46,6 +45,9 @@ func (d *driverServer) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnp
 }
 
 // Implement classic volume plugin interfaces
+func (d *driverServer) Name() string {
+	return "csi"
+}
 func (d *driverServer) Create(req *volumeplugin.CreateRequest) error {
 	ctx := context.Background()
 	_, err := d.CreateVolume(ctx, &csi.CreateVolumeRequest{
@@ -63,6 +65,13 @@ func (d *driverServer) Create(req *volumeplugin.CreateRequest) error {
 				},
 			},
 		},
+	})
+	return err
+}
+func (d *driverServer) Remove(r *volumeplugin.RemoveRequest) error {
+	ctx := context.Background()
+	_, err := d.DeleteVolume(ctx, &csi.DeleteVolumeRequest{
+		VolumeId: r.Name,
 	})
 	return err
 }
@@ -88,46 +97,57 @@ func (d *driverServer) Get(r *volumeplugin.GetRequest) (*volumeplugin.GetRespons
 		},
 	}, nil
 }
-func (d *driverServer) Remove(r *volumeplugin.RemoveRequest) error {
-	ctx := context.Background()
-	_, err := d.DeleteVolume(ctx, &csi.DeleteVolumeRequest{
-		VolumeId: r.Name,
-	})
-	return err
-}
-func (d *driverServer) Path(r *volumeplugin.PathRequest) (*volumeplugin.PathResponse, error) {
-	return &volumeplugin.PathResponse{}, nil
-}
-func (d *driverServer) Mount(r *volumeplugin.MountRequest) (*volumeplugin.MountResponse, error) {
-	return &volume.MountResponse{}, nil
-}
-func (d *driverServer) Unmount(r *volumeplugin.UnmountRequest) error {
-	return nil
-}
-func (d *driverServer) Capabilities() *volume.CapabilitiesResponse {
-	return &volume.CapabilitiesResponse{
-		Capabilities: volume.Capability{Scope: "local"},
+
+/*
+	func (d *driverServer) Path(r *volumeplugin.PathRequest) (*volumeplugin.PathResponse, error) {
+		return &volumeplugin.PathResponse{}, nil
 	}
+
+	func (d *driverServer) Mount(r *volumeplugin.MountRequest) (*volumeplugin.MountResponse, error) {
+		return &volume.MountResponse{}, nil
+	}
+
+	func (d *driverServer) Unmount(r *volumeplugin.UnmountRequest) error {
+		return nil
+	}
+
+	func (d *driverServer) Capabilities() *volume.CapabilitiesResponse {
+		return &volume.CapabilitiesResponse{
+			Capabilities: volume.Capability{Scope: "local"},
+		}
+	}
+*/
+func (d *driverServer) Scope() string {
+	return "local"
 }
 
 func main() {
-	p, err := filepath.Abs(filepath.Join("run", "docker", "plugins"))
-	if err != nil {
-		panic(err)
-	}
-	if err := os.MkdirAll(p, 0o755); err != nil {
-		panic(err)
-	}
-	l, err := net.Listen("unix", filepath.Join(p, "csi.sock"))
-	if err != nil {
-		panic(err)
-	}
-
-	server := grpc.NewServer()
+	/*
+		p, err := filepath.Abs(filepath.Join("run", "docker", "plugins"))
+		if err != nil {
+			panic(err)
+		}
+		if err := os.MkdirAll(p, 0o755); err != nil {
+			panic(err)
+		}
+		l, err := net.Listen("unix", filepath.Join(p, "csi.sock"))
+		if err != nil {
+			panic(err)
+		}
+		server := grpc.NewServer()
+	*/
 	ds := &driverServer{}
-	csi.RegisterIdentityServer(server, ds)
-	csi.RegisterControllerServer(server, ds)
-	csi.RegisterNodeServer(server, ds)
+	/*
+		csi.RegisterIdentityServer(server, ds)
+		csi.RegisterControllerServer(server, ds)
+		csi.RegisterNodeServer(server, ds)
+		server.Serve(l)
+	*/
 
-	server.Serve(l)
+	//driver := &VolumeDriver{}
+	h := shim.NewHandlerFromVolumeDriver(ds)
+	if err := h.ServeUnix("csi", 0); err != nil {
+		log.Errorf("ServeUnix failed: %v", err)
+		return
+	}
 }
